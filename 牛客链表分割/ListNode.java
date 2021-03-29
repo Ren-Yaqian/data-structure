f83b0d8df0f04ffe0b26f23e93e1359ac8fcf4ed
Ren-Yaqian <74707036+Ren-Yaqian@users.noreply.github.com>
2021-03-29 11:35:50 +0800

    Add files via upload

---
+++ 牛客链表分割/ListNode.java
@@ -0,0 +1,12 @@
+package 牛客链表分割;
+
+public class ListNode {
+    public int val; // 元素
+    public ListNode next;   // 指向下一个结点对象
+    // next == null，代表链表的最后一个结点
+
+    public ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
